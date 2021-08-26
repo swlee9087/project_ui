6ff30d3703c22899851f7e0cab7a4777f68d3c05
swlee9087 <sw_lee9087@naver.com>
2021-08-26 13:05:56 +0900

    12

--- src/App.js
+++ src/App.js
@@ -9,39 +9,42 @@ import {Redirect, Route, Switch} from 'react-router-dom'
 // import Home from './components/Home'
 // import Nav from './components/Nav';
 
-import styled from "styled-components";
-import Header from "./Header";
+// import styled from "styled-components";
+// import Header from "./Header";
 
-import NavForJavaScript from './components/NavForJavaScript'
-import Basic from './modern-javascript/01-basic/Basic'
-import ArrowFn from './modern-javascript/02-arrow-fn/ArrowFn'
-import Arguments from './modern-javascript/03-arguments/Arguments'
-import TemplateLiteral from './modern-javascript/04-template-literal/TemplateLiteral'
-import LiteralMethod from './modern-javascript/05-literal-method/LiteralMethod'
-import Destructuring from './modern-javascript/06-destructuring/Destructuring'
-import Loop from './modern-javascript/07-loop/Loop'
-import ArrayMethod from './modern-javascript/08-array-method/ArrayMethod'
-import SpreadOpRestParam from './modern-javascript/09-spread-operator-rest-param/SpreadOpRestParam'
-import ObjLiteral from './modern-javascript/10-obj-literal/ObjLiteral'
-import Symbol from './modern-javascript/11-symbol/Symbol'
-import Class from './modern-javascript/12-class/Class'
-import Promise from './modern-javascript/13-promise/Promise'
-import Generator from './modern-javascript/14-generator/Generator'
-import Proxy from './modern-javascript/15-proxy/Proxy'
-import SetMap from './modern-javascript/16-set-map/SetMap'
-import Array from './modern-javascript/17-array/Array'
-import Es2017 from './modern-javascript/18-es2017/Es2017'
-import AsyncAwait from './modern-javascript/19-async-await/AsyncAwait'
-import Es2018 from './modern-javascript/20-es2018/Es2018'
-import Es2019 from './modern-javascript/21-es2019/Es2019'
-import Es2020 from './modern-javascript/22-es2020/Es2020'
-import TypeScript from './modern-javascript/23-typescript/Typescript'
+// import NavForJavaScript from './components/NavForJavaScript'
+// import Basic from './modern-javascript/01-basic/Basic'
+// import ArrowFn from './modern-javascript/02-arrow-fn/ArrowFn'
+// import Arguments from './modern-javascript/03-arguments/Arguments'
+// import TemplateLiteral from './modern-javascript/04-template-literal/TemplateLiteral'
+// import LiteralMethod from './modern-javascript/05-literal-method/LiteralMethod'
+// import Destructuring from './modern-javascript/06-destructuring/Destructuring'
+// import Loop from './modern-javascript/07-loop/Loop'
+// import ArrayMethod from './modern-javascript/08-array-method/ArrayMethod'
+// import SpreadOpRestParam from './modern-javascript/09-spread-operator-rest-param/SpreadOpRestParam'
+// import ObjLiteral from './modern-javascript/10-obj-literal/ObjLiteral'
+// import Symbol from './modern-javascript/11-symbol/Symbol'
+// import Class from './modern-javascript/12-class/Class'
+// import Promise from './modern-javascript/13-promise/Promise'
+// import Generator from './modern-javascript/14-generator/Generator'
+// import Proxy from './modern-javascript/15-proxy/Proxy'
+// import SetMap from './modern-javascript/16-set-map/SetMap'
+// import Array from './modern-javascript/17-array/Array'
+// import Es2017 from './modern-javascript/18-es2017/Es2017'
+// import AsyncAwait from './modern-javascript/19-async-await/AsyncAwait'
+// import Es2018 from './modern-javascript/20-es2018/Es2018'
+// import Es2019 from './modern-javascript/21-es2019/Es2019'
+// import Es2020 from './modern-javascript/22-es2020/Es2020'
+// import TypeScript from './modern-javascript/23-typescript/Typescript'
+import HomePage from './pages/HomePage';
 
 
 
 const App = () => (
-<><Layout/><Header/>
-<NavForJavaScript/>
+<>
+{/* <Layout/><Header/>
+<NavForJavaScript/> */}
+<HomePage/>
 <Switch>
 
 {/* <Route exact path='/' component ={Home}/>
@@ -52,7 +55,7 @@ const App = () => (
 <Route exact path='/register' component ={CourseRegister}/>
 <Route exact path='/profile' component ={OnlineProfile}/> */}
 
-<Route exact path='/basic' component={Basic}/>
+{/* <Route exact path='/basic' component={Basic}/>
 <Route exact path='/arrowfn' component={ArrowFn}/>
 <Route exact path='/arguments' component={Arguments}/>
 <Route exact path='/templit' component={TemplateLiteral}/>
@@ -74,19 +77,22 @@ const App = () => (
 <Route exact path='/es2018' component={Es2018}/>
 <Route exact path='/es2019' component={Es2019}/>
 <Route exact path='/es2020' component={Es2020}/>
-<Route exact path='/typescript' component={TypeScript}/>
+<Route exact path='/typescript' component={TypeScript}/> */}
+
+<Route exact path='/HomePage' component={HomePage}/>
+
 
 
 </Switch></>
 )
 
-const Layout =styled.div`
-margin: 0 auto;
-padding-left: 100px;
-display: flex;
-width:100%;
-flex-flow:row wrap;
-`
+// const Layout =styled.div`
+// margin: 0 auto;
+// padding-left: 100px;
+// display: flex;
+// width:100%;
+// flex-flow:row wrap;
+// `
 
 
 export default App
---
+++ src/pages/HomePage.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (<>
+    
+    <table style={{width:"1200px", height:"600px", margin:"0 auto", border:"1px solid black"}}>
+        <tr style={{width:"100%", height:"20%", border:"1px solid black"}}>
+            <td style={{width:"20%", border:"1px solid black"}}></td>
+            <td style={{width:"70%", border:"1px solid black"}}></td>
+            <td style={{width:"20%", border:"1px solid black"}}></td>
+        </tr>
+        <tr style={{width:"1200px", height:"600px", margin:"0 auto", border:"1px solid black"}}>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+
+    </>)
+}
+
+export default HomePage
